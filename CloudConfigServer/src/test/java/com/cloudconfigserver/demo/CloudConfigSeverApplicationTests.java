d3fc1030df65be0e66db993ec6da19d85d715cfc
Deeptiwani <Deeptiwani@host.docker.internal>
2024-08-23 11:42:23 +0530

    Initial Commit.

---
+++ CloudConfigServer/src/test/java/com/cloudconfigserver/demo/CloudConfigSeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudconfigserver.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigSeverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
